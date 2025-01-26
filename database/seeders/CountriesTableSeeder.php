d902218896530fac8e9ddb320864b465990c3c32
Ahmed Yousry <63677504+AhmedYousry2020@users.noreply.github.com>
2025-01-26 23:33:04 +0200

    updates

--- database/seeders/CountriesTableSeeder.php
+++ database/seeders/CountriesTableSeeder.php
@@ -1,8 +1,8 @@
 <?php
 
+
 namespace Database\Seeders;
 
-use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
 use Illuminate\Support\Facades\DB;
 
@@ -13,19 +13,21 @@ class CountriesTableSeeder extends Seeder
      */
     public function run(): void
     {
+        // Inserting into the `countries` table
         $query = <<<SQL
         INSERT INTO `countries` (`id`, `flag`, `created_at`, `updated_at`) VALUES
-        (198, 'https://restcountries.eu/data/sau.svg', '2021-07-14 10:03:42', '2021-07-14 10:03:42'),
+        (198, 'https://restcountries.eu/data/sau.svg', '2021-07-14 10:03:42', '2021-07-14 10:03:42');
         SQL;
 
         DB::statement($query);
+
+        // Inserting into the `country_translations` table
         $query1 = <<<SQL
-        INSERT INTO `country_translations` (`id`, `country_id`,`locale`,`name` ,`created_at`, `updated_at`) VALUES
+        INSERT INTO `country_translations` (`id`, `country_id`, `locale`, `name`, `created_at`, `updated_at`) VALUES
         (2, 198, 'en', 'Saudi Arabia', NULL, NULL),
-        (4, 198, 'ar', 'المملكة العربية السعودية', NULL, NULL),
+        (4, 198, 'ar', 'المملكة العربية السعودية', NULL, NULL);
         SQL;
 
         DB::statement($query1);
-
     }
 }
