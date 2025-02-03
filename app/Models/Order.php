50d9bb8e4803d64cb7e72f188695649f4003ee43
Ahmed Yousry <63677504+AhmedYousry2020@users.noreply.github.com>
2025-02-03 19:33:34 +0200

    udapetes

--- app/Models/Order.php
+++ app/Models/Order.php
@@ -22,9 +22,9 @@ class Order extends Model
         return $this->belongsTo(User::class);
     }
 
-    public function getTotalPriceAttribute($value)
-    {
-        $currency = app()->getLocale() === 'ar' ? 'دينار كويتي' : 'KWD';
-        return number_format($value, 2) . ' ' . $currency;
-    }
+    // public function getTotalPriceAttribute($value)
+    // {
+    //     $currency = app()->getLocale() === 'ar' ? 'دينار كويتي' : 'KWD';
+    //     return number_format($value, 2) . ' ' . $currency;
+    // }
 }
